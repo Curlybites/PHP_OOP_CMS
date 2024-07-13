ff20319590ffacffccd1663963899798eac5a296
Anthony Esquilona <91820043+Curlybites@users.noreply.github.com>
2024-07-13 17:16:46 +0800

    first deploy

---
+++ admin/home.php
@@ -0,0 +1,15 @@
+<?php 
+    require_once '../components/header.php';
+?>
+
+
+<?php 
+    require_once '../admin/components/navbar.php';
+?>
+
+
+
+
+<?php 
+    require_once '../components/footer.php';
+?>
