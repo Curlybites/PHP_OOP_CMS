33bdc89cd6532b446e1c46c49796bcc436b7fcbe
Anthony Esquilona <91820043+Curlybites@users.noreply.github.com>
2024-07-13 17:52:56 +0800

    adding admin

--- admin/components/navbar.php
+++ admin/components/navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="home.php">Navbar</a>
+<nav class="navbar navbar-expand-lg bg-body-tertiary ">
+  <div class="container-md ">
+    <a class="navbar-brand" href="home.php">ESKI CMS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,24 +10,17 @@
           <a class="nav-link active" aria-current="page" href="home.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="article.php">Article</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+            <a class="nav-link" href="category.php">Category</a>
         </li>
       
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+    <form class="navbar-nav" role="search">
+        <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
+         <input type="submit" value="Log out" class="btn btn-sm btn-outline-primary">
+        <!-- <button class="btn btn-outline-success" type="submit">Search</button>    -->
       </form>
     </div>
   </div>
--- admin/home.php
+++ admin/home.php
@@ -1,15 +1,40 @@
-<?php 
-    require_once '../components/header.php';
+<?php
+require_once '../components/header.php';
 ?>
 
 
-<?php 
-    require_once '../admin/components/navbar.php';
+<?php
+require_once '../admin/components/navbar.php';
 ?>
 
+<div class="container-fluid">
+
+    <div class=" row  justify-content-center gap-3 my-3">
+        <div class="card col-3">
+            <div class="card-body">
+                <h5 class="card-title">Number of Article</h5>
+                <p class="card-text">20</p>
+            </div>
+        </div>
+        <div class="card col-3">
+            <div class="card-body">
+                <h5 class="card-title">Number of User</h5>
+                <p class="card-text">20</p>
+            </div>
+        </div>
+        <div class="card col-3">
+            <div class="card-body">
+                <h5 class="card-title">Number of Category</h5>
+                <p class="card-text">20</p>
+            </div>
+        </div>
+
+
+    </div>
+</div>
 
 
 
-<?php 
-    require_once '../components/footer.php';
+<?php
+require_once '../components/footer.php';
 ?>
